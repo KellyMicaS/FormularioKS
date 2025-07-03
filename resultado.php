04b640dfdc0f55a5762183748bac1e6e904507d7
Kelly S <kellysolanodl@gmail.com>
2025-07-03 09:21:46 -0500

    Correccion formulario

--- resultado.php
+++ resultado.php
@@ -8,33 +8,28 @@
 <body>
     <h1>Resultado</h1>
     <?php
+
         $nombre= $_POST["nombre"];
-        print ("El estudiante se llama: $nombre");
-        print "<br>"; 
-
         $cedula= $_POST["cedula"];
-        print ("La cédula del estudiante es: $cedula");
-        print "<br>"; 
-
         $materia= $_POST["materia"];
-        print ("Materia: $materia");
-        print "<br>"; 
-
         $num1= $_POST["num1"];
-        print ("Parcial 1: $num1");
-        print "<br>";
-
         $num2= $_POST["num2"];
-        print ("Parcial 2: $num2");
-        print "<br>";
-
         $num3= $_POST["num3"];
-        print ("Parcial 3: $num3");
-        print "<br>";
-
         $promedio= ($num1+$num2+$num3)/3;
-        print ("Promedio general de $materia: $promedio");
-        print "<br>";
+
+        print ("<br> El estudiante se llama: $nombre");
+
+        print ("<br> La cédula del estudiante es: $cedula");
+
+        print ("<br> Materia: $materia");
+
+        print ("<br> Parcial 1: $num1");
+
+        print ("<br> Parcial 2: $num2");
+
+        print ("<br> Parcial 3: $num3");
+
+        print ("<br> Promedio general de $materia: ". number_format($promedio,2));
     ?>
 </body>
 </html>
