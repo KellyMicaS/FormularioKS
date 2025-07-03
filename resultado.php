3fa5b18955e57bba48025949c9cd14a58c9be383
Kelly S <kellysolanodl@gmail.com>
2025-07-03 09:37:25 -0500

    Promedio Calculado

--- resultado.php
+++ resultado.php
@@ -9,6 +9,7 @@
     <h1>Resultado</h1>
     <?php
 
+        //Ingreso, declaración y procesamiento de variables recibidas desde el formulario
         $nombre= $_POST["nombre"];
         $cedula= $_POST["cedula"];
         $materia= $_POST["materia"];
@@ -16,6 +17,8 @@
         $num2= $_POST["num2"];
         $num3= $_POST["num3"];
         $promedio= ($num1+$num2+$num3)/3;
+
+        //Impresión de la información del Front
 
         print ("<br> El estudiante se llama: $nombre");
 
@@ -29,7 +32,17 @@
 
         print ("<br> Parcial 3: $num3");
 
-        print ("<br> Promedio general de $materia: ". number_format($promedio,2));
+        //Verificación del estado del promedio
+
+        if($promedio >= 7 && $promedio <= 10){
+            print("<br> $nombre pasa de año en $materia con un promedio de: " . number_format($promedio,2));
+        }else if($promedio >= 3.6 && $promedio <= 6.9){
+            print("<br> $nombre está suspenso en $materia con un promedio de: " . number_format($promedio,2));
+        }else if($promedio >= 0 && $promedio <= 3.5 ){
+            print("<br> $nombre pierde el año en $materia con un promedio de: " . number_format($promedio,2));
+        }
     ?>
+    <br>
+    <a href="index.html">Regresar al inicio</a>
 </body>
 </html>
